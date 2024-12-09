953aab3477cb07b69d1c44c710b63ad3f48aa79c
Supakorn4623 <126290832+Supakorn4623@users.noreply.github.com>
2024-12-09 13:59:53 +0700

    Update models.py

--- webproject/E/models.py
+++ webproject/E/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class course(models.Model):
+  course_id = models.IntegerField(unique=True)
+  course_name = models.CharField(max_length=100)
+  course_teacher= models.CharField(max_length=100)
